hissy/bedrock

commit c964cdf673e803c53f47e4830725fa5369c317ae

Make sure unchecked nodes stay unchecked
Make sure unchecked child nodes stay unchecked after expanding and collapsing a node.

assets/cms/js/tree.js
@@ -229,6 +229,14 @@ ConcreteTree.prototype = {
                     });
                 }
             },
+            collapse: function(event, data) {
+                //loop over child nodes and check if node is still selected. If not remove it from the 'options.ajaxData.selected' array.
+                data.node.children.forEach(function(nodeChild) {
+                    if (options.ajaxData.selected.includes(nodeChild.key) && !nodeChild.isSelected()) {
+                        delete options.ajaxData.selected.splice(options.ajaxData.selected.indexOf(nodeChild.key), 1);
+                    }
+                });
+            },
             dnd: {
                 preventRecursiveMoves: true, // Prevent dropping nodes on own descendants,
                 focusOnClick: true,

commit 28892ebe6f59d7102bb77516d70e50979794e0b6

Fix "Cannot read properties of undefined" error
"selected" was introduced in c964cdf673e803c53f47e4830725fa5369c317ae ; This property doesn't exist in the ajaxData. We may remove this function or should rename it to "treeNodeSelectedIDs", but keep it as it is to avoid unexpected side effects.

assets/cms/js/tree.js
@@ -226,11 +226,13 @@ ConcreteTree.prototype = {
             },
             collapse: function(event, data) {
                 // loop over child nodes and check if node is still selected. If not remove it from the 'options.ajaxData.selected' array.
+                if (options.ajaxData.selected) {
                 data.node.children.forEach(function(nodeChild) {
                     if (options.ajaxData.selected.includes(parseInt(nodeChild.key)) && !nodeChild.isSelected()) {
                         options.ajaxData.selected.splice(options.ajaxData.selected.indexOf(nodeChild.key), 1)
                     }
                 })
+                }
             },
             dnd: {
                 preventRecursiveMoves: true, // Prevent dropping nodes on own descendants,